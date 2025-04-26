3bbd5cdd53a55231f9ca2fc95d31b5ec640ca1a0
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-26 09:40:37 +0500

    Functions: Declare A Function (Fixed)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ iteration = int(input())
 
 def repeat():
     sumation = 0
-    for i in range(1, 1001):
+    for i in range(1, 10001):
         sumation+=i
     for i in range(iteration):
         print(sumation)
