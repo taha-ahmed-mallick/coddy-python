57d64e274788b3c4d71c844dc2194c696100e58f
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-26 14:44:55 +0500

    FizzBuzz with a Twist: The FizzBuzz Function

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print("Welcome to FizzBuzz!")
+def fizzbuzz(n):
+    if n%21==0:
+        return "FizzBuzz"
+    else:
+        if n%7==0:
+            return "Buzz"
+        elif n%3==0:
+            return "Fizz"
+        else:
+            return f"{n}"
+
+usr_n = int(input())
+result = fizzbuzz(usr_n)
+print(result)
