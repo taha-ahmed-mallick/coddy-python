3ee05819c08379abba3e03c47bb29484a92394b6
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-26 14:37:39 +0500

    FizzBuzz with a Twist: Game Overview

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-def is_valid(username, password):
-    # Write your code below
-    if username == "admin":
-        return True
-    elif username== "user" and password == "qweasd":
-        return True
-    else:
-        return False
-
-usr_name = input()
-password = input()
-# Call the function below
-result = is_valid(usr_name, password)
-print(result)
+print("Welcome to FizzBuzz!")
