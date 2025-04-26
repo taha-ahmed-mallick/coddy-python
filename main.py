57504c62e4e182edd1ba844f8b25e575708d1b37
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-26 21:38:44 +0500

    List Basics: Accessing List Elements

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-shopping_list = ["bread", "eggs", "milk", "butter"]
+def values(lst):
+    # Write code here
+    for i in range(len(lst)):
+        print(lst[i])
+    pass
+
+usr_lst = input().split(",")
+values(usr_lst)
