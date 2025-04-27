37513a28c8ab907ca29b8c98bba46018cb737dff
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-27 15:22:50 +0500

    List Advanced: List Slicing Part 2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 lst = input().split(",")
 # Write your code below
-half = len(lst)//2
-if len(lst)%2==0:
-    print(lst[half-1:half+1])
-else:
-    print(lst[half-1:half+2])
+# list containing every 3rd element from index 1
+print(lst[1::3])
+# list containing every element from 6th to 1st in reverse order
+print(lst[5-len(lst):-len(lst)-1:-1])
+# list containing every second element starting from the middle of the list to the end
+print(lst[len(lst)//2::2])
