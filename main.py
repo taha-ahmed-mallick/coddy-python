fd40affe8fe48fbcf1dd281721edbfeba728f8c6
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-27 08:38:20 +0500

    Iterating Over Sequences: The Enumerate Function

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-lst = input().split(",")
+lst = list(map(int, input().split(",")))
 # Write your code below
 updated=[]
-for items in lst:
-    if len(items)>5:
-        updated.append(items)
+for index, num in enumerate(lst):
+    if num < 50 or num%5==0:
+        updated.append(index)
 
 print(updated)
