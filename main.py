cc76e6944683d7845372cf36ffa5ea3a94079810
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-27 14:59:16 +0500

    List Advanced: List Slicing Part 1

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-text = input()
-delimiter = input()
+lst = input().split(",")
 # Write your code below
-text = text.split()
-
-text = delimiter.join(text)
-
-print(text)
+half = len(lst)//2
+if len(lst)%2==0:
+    print(lst[half-1:half+1])
+else:
+    print(lst[half-1:half+2])
