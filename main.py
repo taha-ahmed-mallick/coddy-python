7bfc65e5556bba582bd9f1a5f8324c60f2858f5f
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-28 10:11:22 +0500

    Final Challenges: Flat Asterisk Pyramid

--- main.py
+++ main.py
@@ -1,42 +1,5 @@
-print("Welcome to the Daily Expense Tracker!")
-print(
-    """
-Menu:
-1. Add a new expense
-2. View all expenses
-3. Calculate total and average expense
-4. Clear all expenses
-5. Exit"""
-)
+n = int(input())
+lines = int((n+1)/2)
 
-expenses = []
-
-while True:
-    choice = int(input())
-    if choice == 1:
-        expense = float(input())
-        expenses.append(expense)
-        print("Expense added successfully!")
-    elif choice == 2:
-        if len(expenses) != 0:
-            print("Your expenses:")
-            for i, expense in enumerate(expenses):
-                print(f"{i+1}. {expense}")
-        else:
-            print("No expenses recorded yet.")
-    elif choice == 3:
-        if len(expenses) != 0:
-            summation = sum(expenses)
-            avg = summation / len(expenses)
-            print(f"Total expense: {summation}")
-            print(f"Average expense: {avg}")
-        else:
-            print("No expenses recorded yet.")
-    elif choice == 4:
-        expenses.clear()
-        print("All expenses cleared.")
-    elif choice == 5:
-        print("Exiting the Daily Expense Tracker. Goodbye!")
-        break
-    else:
-        print("Invalid choice. Please try again.")
+for i in range(lines):
+    print("*"*(2*(i+1)-1))
