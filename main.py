0474e9c7464e516f342354565ec99ab128c609ea
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-28 09:32:08 +0500

    Daily Expense Tracker: Add, View & Exit

--- main.py
+++ main.py
@@ -8,3 +8,22 @@ Menu:
 4. Clear all expenses
 5. Exit"""
 )
+
+expenses = []
+
+while True:
+    choice = int(input())
+    if choice == 1:
+        expense = float(input())
+        expenses.append(expense)
+        print("Expense added successfully!")
+    elif choice == 2:
+        if len(expenses) != 0:
+            print("Your expenses:")
+            for i, expense in enumerate(expenses):
+                print(f"{i+1}. {expense}")
+        else:
+            print("No expenses recorded yet.")
+    elif choice == 5:
+        print("Exiting the Daily Expense Tracker. Goodbye!")
+        break
