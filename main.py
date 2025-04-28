0309e74cf4b95cc33192600cb76e855db6d60060
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-28 09:17:06 +0500

    Daily Expense Tracker: Project Overview (Fixed)

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 print("Welcome to the Daily Expense Tracker!")
-print("""Menu:
+print(
+    """
+Menu:
 1. Add a new expense
 2. View all expenses
 3. Calculate total and average expense
 4. Clear all expenses
-5. Exit""")
+5. Exit"""
+)
