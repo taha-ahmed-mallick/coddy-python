25b4886b4dfdd1455445dc0174cd52e1dccf440c
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-28 17:38:13 +0500

    Variables Exploration: Constants

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-prices = input().split(",")
-for i in range(len(prices)):
-    prices[i] = int(prices[i])
-items = input().split(",")
-budget_per_item = int(input())
-
-affordable_items = []
-cant_afford = 0
-total_needed = 0
-
-
-# Write your code below
-for i, price in enumerate(prices):
-    if price <= budget_per_item:
-        affordable_items.append(items[i])
-        total_needed += price
-    else:
-        cant_afford += 1
-
-
-print("Can buy:", affordable_items)
-print("Total budget needed:", total_needed)
-print("Can't afford:", cant_afford)
+# Write code here
+PI = 3.14159
+radius = float(input())
+print(PI * radius**2)
