8812f4c428969183cdd840278f4cd9183bda3c4a
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-28 17:20:30 +0500

    Final Challenges: Pattern Finder

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-n = int(input())
-lines = int((n+1)/2)
+def find_occurrences(text, pattern):
+    # Write your code here
+    was_found = False
+    occurrence = 0
+    indices = []
+    for i, char in enumerate(text):
+        if pattern[0] == char:
+            pattern_found = True
+            for j, char_ in enumerate(pattern):
+                if i + j <= len(text) - 1:
+                    if char_ != text[i + j]:
+                        pattern_found = False
+                        break
+                else:
+                    pattern_found = False
+            if pattern_found:
+                occurrence += 1
+                indices.append(i)
+    if occurrence:
+        was_found = True
+    return (was_found, occurrence, indices)
 
-for i in range(lines):
-    print("*"*(2*(i+1)-1))
+
+# Read input
+text = input()
+pattern = input()
+
+# Call your function and print the result
+result = find_occurrences(text, pattern)
+print(result)
