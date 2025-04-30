4316c97112b88c7475a89ca4d255ef2bc6393a32
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-30 14:09:53 +0500

    Variable Exploration: Placeholder Variables

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-# Loop 5 times using a placeholder variable
-for _ in range(5):
-    print("Iteration")
-    
-# List of numbers
-numbers = [10, 20, 30, 40, 50]
-
-# Unpack the list using placeholder variables
-first, _, middle, _, last = numbers
-
-# Print the values of first, middle, and last
-print(f"First: {first}")
-print(f"Middle: {middle}")
-print(f"Last: {last}")
+# Write code here
+usr_in = float(input())
+dec_places = int(input())
+print(round(usr_in, dec_places))
