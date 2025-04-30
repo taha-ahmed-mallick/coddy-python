f9090a00721072cc621ac0cd4bf745dedbb47e1a
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-04-30 14:27:46 +0500

    Dictionaries Part 1: Creating A Dictionary

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-# Write code here
-lst1 = list((10,20,30))
-lst2 = list("python")
-lst3 = list(range(1,6))
-print(lst1)
-print(lst2)
-print(lst3)
+def create_student_dict(name, age, major):
+    # Write code here
+    student = {
+        "name": name,
+        "age": age,
+        "major": major
+    }
+    return student
+
+name = input()
+age = int(input())
+major = input()
+print(create_student_dict(name, age, major))
